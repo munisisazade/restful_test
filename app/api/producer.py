2bc5f1d2320e6c22901f996bdb319e55a45ee781
Munis Isazade <munisisazade@gmail.com>
2021-02-03 12:23:48 +0400

    feat: Body service add

--- app/api/producer.py
+++ app/api/producer.py
@@ -24,7 +24,7 @@ def rest_header_checker():
 
 
 @micro_api.route('/api/v1/body/example', methods=['GET'])
-def rest_header_checker():
+def rest_body_checker():
     return {
         "request.data": str(request.data)
     }
