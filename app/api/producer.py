ae6208a8c5d3e2add365f7fac2472de219d19a5e
Munis Isazade <munisisazade@gmail.com>
2021-07-23 16:32:30 +0400

    Update

--- app/api/producer.py
+++ app/api/producer.py
@@ -745,7 +745,38 @@ def example_soap_service():
   </wsdl:service>
 </wsdl:definitions>""", mimetype="text/xml")
     else:
-        return Response(response='<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Body><SubtractResponse xmlns="http://tempuri.org/"><SubtractResult>10</SubtractResult></SubtractResponse></soap:Body></soap:Envelope>', mimetype="text/xml")
+        return Response(response="""<?xml version="1.0" encoding="UTF-8"?>
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+    <soapenv:Header/>
+    <soapenv:Body>
+       <GetAppinfoByPINResponse>
+          <Applications>
+             <Item>
+                <element>
+                   <CountryCode>99</CountryCode>
+                   <CountryName>dfhdsf df hgdfhfh</CountryName>
+                   <DecisionDate>2020-11-06T00:00:00.000Z</DecisionDate>
+                   <DecisionNumber>23-23-364</DecisionNumber>
+                   <EducationLevel>10</EducationLevel>
+                   <EducationLevelName>Bakalavriat</EducationLevelName>
+                   <Fullname>dfg sdgfdfsgfgdsfg</Fullname>
+                   <ICertificateNumber>sdfsdf</ICertificateNumber>
+                   <PIN>asfdsdfsdf</PIN>
+                   <SpecialtyCode>sdfsdf</SpecialtyCode>
+                   <SpecialtyName>Menecment</SpecialtyName>
+                   <TCertificateNumber>sdfsdf</TCertificateNumber>
+                   <UniversitetCode>sdfsdf</UniversitetCode>
+                   <UniversitetName>Ardahan sdfsdf</UniversitetName>
+                </element>
+             </Item>
+          </Applications>
+          <Status>
+             <Code>200</Code>
+             <Description>Uğurlu.</Description>
+          </Status>
+       </GetAppinfoByPINResult>
+       </soapenv:Body>
+    </soapenv:Envelope>""", mimetype="text/xml")
 
 
 
